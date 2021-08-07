24a2a4d7a5a0c811d98352b3f8a45d8c3e2130e2
ueqt <ueqtxu@gmail.com>
2021-08-07 15:28:07 +0800

    add failover clustering

--- S10-WAC.ps1
+++ S10-WAC.ps1
@@ -5,6 +5,9 @@ Start-BitsTransfer -Source https://aka.ms/WACDownload -Destination "$env:USERPRO
 
 Start-Process msiexec.exe -Wait -ArgumentList "/i $env:USERPROFILE\Downloads\WindowsAdminCenter.msi /qn /L*v log.txt REGISTRY_REDIRECT_PORT_80=1 SME_PORT=443 SSL_CERTIFICATE_OPTION=generate"
 
+Enable-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V -Online -NoRestart 
+Install-WindowsFeature -Name "Failover-Clustering","RSAT-Clustering-Powershell","Hyper-V-PowerShell"
+
 # you can use edge access https://localhost to access WAC
 # in WAC, Cluster Manager add AzSHci-Cluster
 # update plugins
