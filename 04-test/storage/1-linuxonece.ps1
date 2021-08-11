bed3090f0ed08e16a0ce388a91f8ce99a38c1bf5
unknown <LabAdmin@CORP.CONTOSO.COM>
2021-08-11 09:19:36 +0000

    add readwriteonce test

--- 04-test/storage/1-linuxonece.ps1
+++ 04-test/storage/1-linuxonece.ps1
@@ -1,12 +1,23 @@
 # https://docs.microsoft.com/en-us/azure-stack/aks-hci/persistent-volume
 # https://docs.microsoft.com/en-us/azure-stack/aks-hci/container-storage-interface-disks
 
-kubectl create -f ./yaml/linuxonce.yaml
+# failed to provision volume with StorageClass "default": rpc error: code = DeadlineExceeded desc = context deadline exceeded
+# restart VM and rerun
+
+kubectl apply -f ./yaml/linuxonce.yaml
 
 kubectl get storageclass
 
 kubectl get pvc
 
+kubectl describe pvc linuxonce-pvc
+
 kubectl get pods -o wide
+
+kubectl exec -it linuxonce-pvc /bin/bash
+
+# cd /data
+# touch 1.txt
+# ls
 
 # kubectl delete -f ./yaml/linuxonce.yaml
---
+++ 04-test/storage/2-windowsonce.ps1
@@ -0,0 +1,21 @@
+# https://docs.microsoft.com/en-us/azure-stack/aks-hci/persistent-volume
+
+kubectl apply -f ./yaml/windowsonce.yaml
+
+kubectl get storageclass
+
+kubectl get pvc
+
+kubectl describe pvc windowsonce-pvc
+
+kubectl get pods -o wide
+
+# kubectl exec -it <win-webserver-xxx> cmd.exe
+
+# dir
+# cd /mnt
+# cd akshciscsi
+# echo "1" > 1.txt
+# dir
+
+# kubectl delete -f ./yaml/windowsonce.yaml
