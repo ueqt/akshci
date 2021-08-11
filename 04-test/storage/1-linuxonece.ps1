273de515bb8529432e03d60bdd8ba3c6feacdfab
unknown <LabAdmin@CORP.CONTOSO.COM>
2021-08-11 07:12:21 +0000

    test

--- 04-test/storage/1-linuxonece.ps1
+++ 04-test/storage/1-linuxonece.ps1
@@ -1,5 +1,12 @@
 # https://docs.microsoft.com/en-us/azure-stack/aks-hci/persistent-volume
+# https://docs.microsoft.com/en-us/azure-stack/aks-hci/container-storage-interface-disks
 
 kubectl create -f ./yaml/linuxonce.yaml
 
-# kubectl uninstall ./yaml/linuxonce.yaml
+kubectl get storageclass
+
+kubectl get pvc
+
+kubectl get pods -o wide
+
+# kubectl delete -f ./yaml/linuxonce.yaml
