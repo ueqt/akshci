45435417024238f6668a792970c3a2429010e7cd
unknown <LabAdmin@CORP.CONTOSO.COM>
2021-08-13 14:07:51 +0000

    fix

--- 04-test/storage/5-cifs.ps1
+++ 04-test/storage/5-cifs.ps1
@@ -1,10 +1,10 @@
 # https://github.com/kubernetes-csi/csi-driver-smb/blob/master/docs/csi-debug.md
 # https://github.com/Azure/aks-hci/issues/116
 
-kubectl get po -o wide -n kube-system | grep csi-smb-controller
+kubectl get po -o wide -n kube-system | grep csi-smb-node
 
 # check cifs mount inside driver
 kubectl exec csi-smb-node-xxxx -n kube-system -c smb -- mount | grep cifs
 
 # if not exist
-kubectl exec csi-smb-controller-xxxx -n kube-system -c smb -- apt install -y cifs-utils
+kubectl exec csi-smb-node-xxxx -n kube-system -c smb -- apt install -y cifs-utils
